2008c1cce6de0805e5b5c59dd26725f167377a2a
Jatin Dhameliya <jatin.scalelot@gmail.com>
2022-11-23 12:54:02 +0530

    update superadmin apis for items events organizer categories discount organizer

--- routes/superadmins/eventcategories.js
+++ routes/superadmins/eventcategories.js
@@ -12,69 +12,79 @@ router.post('/', helper.authenticateToken, async (req, res) => {
         const { page, limit, search, sortfield, sortoption } = req.body;
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let superadmin = await primary.model(constants.MODELS.superadmins, superadminModel).findById(req.token.superadminid).lean();
-        if(superadmin){
+        if (superadmin) {
             primary.model(constants.MODELS.eventcategories, eventcategoryModel).paginate({
                 $or: [
-                    { categoryname : { '$regex' : new RegExp(search, "i") } },
-                    { description : { '$regex' : new RegExp(search, "i") } }
+                    { categoryname: { '$regex': new RegExp(search, "i") } },
+                    { description: { '$regex': new RegExp(search, "i") } }
                 ]
-            },{
+            }, {
                 page,
                 limit: parseInt(limit),
-                sort: { [sortfield] : [sortoption] },
+                sort: { [sortfield]: [sortoption] },
                 lean: true
             }).then((categories) => {
                 return responseManager.onSuccess('Categories list!', categories, res);
             }).catch((error) => {
                 return responseManager.onError(error, res);
             })
-        }else{
+        } else {
             return responseManager.unauthorisedRequest(res);
         }
-    }else{
+    } else {
         return responseManager.badrequest({ message: 'Invalid token to get categories list, please try again' }, res);
     }
 });
 router.post('/save', helper.authenticateToken, async (req, res) => {
     if (req.token.superadminid && mongoose.Types.ObjectId.isValid(req.token.superadminid)) {
-        const { categoryid, categoryname, description, status } = req.body;
-        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-        let superadmin = await primary.model(constants.MODELS.superadmins, superadminModel).findById(req.token.superadminid).lean();
-        if(superadmin){
-            if(categoryid && categoryid != '' && mongoose.Types.ObjectId.isValid(categoryid)){
-                let existingCategory = await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findOne({_id : {$ne : categoryid}, categoryname : categoryname}).lean();
-                if(existingCategory == null){
-                    let obj = {
-                        categoryname : categoryname,
-                        description : description,
-                        status : status,
-                        updatedBy : mongoose.Types.ObjectId(req.token.superadminid)
-                    };
-                    await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findByIdAndUpdate(categoryid, obj);
-                    return responseManager.onSuccess('Category updated sucecssfully!', 1, res);
-                }else{
-                    return responseManager.badrequest({ message: 'Category name can not be identical, please try again' }, res);
+        const { categoryid, categoryname, description, event_type, status } = req.body;
+        if (event_type) {
+            if (event_type == 'b2b' || event_type == 'public_party') {
+                let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+                let superadmin = await primary.model(constants.MODELS.superadmins, superadminModel).findById(req.token.superadminid).lean();
+                if (superadmin) {
+                    if (categoryid && categoryid != '' && mongoose.Types.ObjectId.isValid(categoryid)) {
+                        let existingCategory = await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findOne({ _id: { $ne: categoryid }, categoryname: categoryname }).lean();
+                        if (existingCategory == null) {
+                            let obj = {
+                                categoryname: categoryname,
+                                description: description,
+                                event_type: event_type,
+                                status: status,
+                                updatedBy: mongoose.Types.ObjectId(req.token.superadminid)
+                            };
+                            await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findByIdAndUpdate(categoryid, obj);
+                            return responseManager.onSuccess('Category updated sucecssfully!', 1, res);
+                        } else {
+                            return responseManager.badrequest({ message: 'Category name can not be identical, please try again' }, res);
+                        }
+                    } else {
+                        let existingCategory = await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findOne({ categoryname: categoryname }).lean();
+                        if (existingCategory == null) {
+                            let obj = {
+                                categoryname: categoryname,
+                                description: description,
+                                event_type: event_type,
+                                status: status,
+                                createdBy: mongoose.Types.ObjectId(req.token.superadminid),
+                                updatedBy: mongoose.Types.ObjectId(req.token.superadminid)
+                            };
+                            await primary.model(constants.MODELS.eventcategories, eventcategoryModel).create(obj);
+                            return responseManager.onSuccess('Category created sucecssfully!', 1, res);
+                        } else {
+                            return responseManager.badrequest({ message: 'Category name can not be identical, please try again' }, res);
+                        }
+                    }
+                } else {
+                    return responseManager.unauthorisedRequest(res);
                 }
-            }else{
-                let existingCategory = await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findOne({categoryname : categoryname}).lean();
-                if(existingCategory == null) {
-                    let obj = {
-                        categoryname : categoryname,
-                        description : description,
-                        status : status,
-                        createdBy : mongoose.Types.ObjectId(req.token.superadminid),
-                        updatedBy : mongoose.Types.ObjectId(req.token.superadminid)
-                    };
-                    await primary.model(constants.MODELS.eventcategories, eventcategoryModel).create(obj);
-                    return responseManager.onSuccess('Category created sucecssfully!', 1, res);
-                }else{
-                    return responseManager.badrequest({ message: 'Category name can not be identical, please try again' }, res);
-                }
+            } else {
+                return responseManager.badrequest({ message: 'Invalid event type for category it can be b2b or public_party only, please try again' }, res);
             }
-        }else{
-            return responseManager.unauthorisedRequest(res);
+        } else {
+            return responseManager.badrequest({ message: 'Invalid event type for category, please try again' }, res);
         }
-    }else{
+    } else {
         return responseManager.badrequest({ message: 'Invalid token to save category data, please try again' }, res);
     }
 });
@@ -83,17 +93,17 @@ router.post('/remove', helper.authenticateToken, async (req, res) => {
         const { categoryid } = req.body;
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let superadmin = await primary.model(constants.MODELS.superadmins, superadminModel).findById(req.token.superadminid).lean();
-        if(superadmin){
-            if(categoryid && categoryid != '' && mongoose.Types.ObjectId.isValid(categoryid)){
+        if (superadmin) {
+            if (categoryid && categoryid != '' && mongoose.Types.ObjectId.isValid(categoryid)) {
                 await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findOneAndRemove(categoryid);
                 return responseManager.onSuccess('Category removed sucecssfully!', 1, res);
-            }else{
+            } else {
                 return responseManager.badrequest({ message: 'Invalid category id to remove category data, please try again' }, res);
             }
-        }else{
+        } else {
             return responseManager.unauthorisedRequest(res);
         }
-    }else{
+    } else {
         return responseManager.badrequest({ message: 'Invalid token to remove category data, please try again' }, res);
     }
 });
@@ -101,16 +111,16 @@ router.post('/list', helper.authenticateToken, async (req, res) => {
     if (req.token.superadminid && mongoose.Types.ObjectId.isValid(req.token.superadminid)) {
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let superadmin = await primary.model(constants.MODELS.superadmins, superadminModel).findById(req.token.superadminid).lean();
-        if(superadmin){
+        if (superadmin) {
             primary.model(constants.MODELS.eventcategories, eventcategoryModel).find({}).then((categories) => {
                 return responseManager.onSuccess('Categories list!', categories, res);
             }).catch((error) => {
                 return responseManager.onError(error, res);
             })
-        }else{
+        } else {
             return responseManager.unauthorisedRequest(res);
         }
-    }else{
+    } else {
         return responseManager.badrequest({ message: 'Invalid token to get categories list, please try again' }, res);
     }
 });
@@ -119,17 +129,17 @@ router.post('/getone', helper.authenticateToken, async (req, res) => {
         const { categoryid } = req.body;
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let superadmin = await primary.model(constants.MODELS.superadmins, superadminModel).findById(req.token.superadminid).lean();
-        if(superadmin){
-            if(categoryid && itemid != '' && mongoose.Types.ObjectId.isValid(categoryid)){
+        if (superadmin) {
+            if (categoryid && itemid != '' && mongoose.Types.ObjectId.isValid(categoryid)) {
                 let categoryData = await primary.model(constants.MODELS.eventcategories, eventcategoryModel).findById(categoryid);
                 return responseManager.onSuccess('Category data !', categoryData, res);
-            }else{
+            } else {
                 return responseManager.badrequest({ message: 'Invalid category id to get item data, please try again' }, res);
             }
-        }else{
+        } else {
             return responseManager.unauthorisedRequest(res);
         }
-    }else{
+    } else {
         return responseManager.badrequest({ message: 'Invalid token to get category data, please try again' }, res);
     }
 });
