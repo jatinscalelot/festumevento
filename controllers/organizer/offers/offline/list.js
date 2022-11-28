7ce9cb26351d180cd6bfb0b58a4c1cf3a765d47a
Jatin Dhameliya <jatin.scalelot@gmail.com>
2022-11-28 15:14:16 +0530

    shop online offer and offline offer controllers created

--- controllers/organizer/offers/offline/list.js
+++ controllers/organizer/offers/offline/list.js
@@ -1,9 +1,9 @@
-const mongoConnection = require('../../utilities/connections');
-const responseManager = require('../../utilities/response.manager');
-const constants = require('../../utilities/constants');
-const organizerModel = require('../../models/organizers.model');
+const mongoConnection = require('../../../../utilities/connections');
+const responseManager = require('../../../../utilities/response.manager');
+const constants = require('../../../../utilities/constants');
+const organizerModel = require('../../../../models/organizers.model');
+const offlineofferModel = require('../../../../models/offlineoffers.model');
 const mongoose = require('mongoose');
-const offlineofferModel = require('../../models/offlineoffers.model');
 exports.list = async (req, res) => {
     if (req.token.organizerid && mongoose.Types.ObjectId.isValid(req.token.organizerid)) {
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
