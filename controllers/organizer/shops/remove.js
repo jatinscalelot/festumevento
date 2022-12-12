a2cf30c3a6946ba3dbe121f4469410f0ef9f7737
Jatin Dhameliya <jatin.scalelot@gmail.com>
2022-12-12 11:38:03 +0530

    update offline offer image to 10 mb max size

--- controllers/organizer/shops/remove.js
+++ controllers/organizer/shops/remove.js
@@ -3,6 +3,7 @@ const organizerModel = require('../../../models/organizers.model');
 const responseManager = require('../../../utilities/response.manager');
 const mongoConnection = require('../../../utilities/connections');
 const constants = require('../../../utilities/constants');
+const offlineofferModel = require('../../../models/offlineoffers.model');
 const mongoose = require('mongoose');
 exports.remove = async (req, res) => {
     if (req.token.organizerid && mongoose.Types.ObjectId.isValid(req.token.organizerid)) {
@@ -12,6 +13,7 @@ exports.remove = async (req, res) => {
             const { shopid } = req.body;
             if (shopid && shopid != '' && mongoose.Types.ObjectId.isValid(shopid)) {
                 await primary.model(constants.MODELS.shops, shopModel).findByIdAndRemove(shopid);
+                await primary.model(constants.MODELS.offlineoffers, offlineofferModel).deleteMany({shopid : mongoose.Types.ObjectId(shopid)});
                 return responseManager.onSuccess('Shop data removed successfully!', 1, res);
             } else {
                 return responseManager.badrequest({ message: 'Invalid shop id to remove Shop, please try again' }, res);
