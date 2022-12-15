edc167491f4b79216d9734ca318dd2d0dd12644c
Jatin Dhameliya <jatin.scalelot@gmail.com>
2022-12-15 17:58:03 +0530

    update async for online offer

--- controllers/organizer/offers/online/list.js
+++ controllers/organizer/offers/online/list.js
@@ -10,13 +10,22 @@ exports.list = async (req, res) => {
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let organizerData = await primary.model(constants.MODELS.organizers, organizerModel).findById(req.token.organizerid).select('-password').lean();
         if (organizerData && organizerData.status == true && organizerData.mobileverified == true) {
-            const { page, limit, search } = req.body;
+            const { page, limit, search, platform } = req.body;
+            let query = {};
+            if(platform && platform != '' && mongoose.Types.ObjectId.isValid(platform)){
+                query = {
+                    product_links : {
+                        platform : mongoose.Types.ObjectId(platform)
+                    }
+                };
+            }
             primary.model(constants.MODELS.onlineoffers, onlineofferModel).paginate({
                 $or: [
                     { offer_title: { '$regex': new RegExp(search, "i") } },
                     { description: { '$regex': new RegExp(search, "i") } }
                 ],
-                createdBy: mongoose.Types.ObjectId(req.token.organizerid)
+                createdBy: mongoose.Types.ObjectId(req.token.organizerid),
+                ...query
             }, {
                 page,
                 limit: parseInt(limit),
