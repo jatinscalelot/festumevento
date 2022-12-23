9e51c8115b0e56bd3b58a42574256e1fed6ccdb4
Jatin Dhameliya <jatin.scalelot@gmail.com>
2022-12-23 11:43:09 +0530

    entrertainment api update

--- controllers/organizer/notifications/payment.js
+++ controllers/organizer/notifications/payment.js
@@ -75,10 +75,54 @@ exports.paynow = async (req, res) => {
                     } else if (notificationData.usertype && notificationData.usertype == 'existingusers') {
                         let numberofusers = await primary.model(constants.MODELS.customerimports, customerimportModel).countDocuments({ notificationid: mongoose.Types.ObjectId(notificationid), selected: true });
                         let defaultSetting = await primary.model(constants.MODELS.settings, settingModel).find({}).lean();
-                        if (defaultSetting && defaultSetting.length > 0) {
-                            return responseManager.onSuccess('Promotion schedule set successfully', {settings : defaultSetting, numberofusers : numberofusers}, res);
-                        } else {
-                            return responseManager.badrequest({ message: 'Something went wrong, please try again' }, res);
+                        if(numberofusers && numberofusers != '' && !isNaN(numberofusers) && parseInt(numberofusers) > 0){
+                            if (defaultSetting && defaultSetting.length > 0) {
+                                if(notificationData.is_notification){
+                                    bk_notificationcost = parseFloat(parseInt(numberofusers) * parseFloat(defaultSetting.notificationcost));
+                                }
+                                if(notificationData.is_email){
+                                    bk_emailcost = parseFloat(parseInt(numberofusers) * parseFloat(defaultSetting.emailcost));
+                                }
+                                if(notificationData.is_sms){
+                                    bk_smscost = parseFloat(parseInt(numberofusers) * parseFloat(defaultSetting.smscost));
+                                }
+                                let bk_total = parseFloat(bk_notificationcost + bk_emailcost + bk_smscost);
+                                if(discount_coupon && discount_coupon != '' && mongoose.Types.ObjectId.isValid(discount_coupon)){
+                                    let discountData = await primary.model(constants.MODELS.notificationcoupons, notificationcouponModel).findById(discount_coupon).lean();
+                                    if (discountData){
+                                        if(discountData.amount && discountData.amount != '' && discountData.amount != 0 && !isNaN(discountData.amount)){
+                                            bk_total = bk_total - parseFloat(discountData.amount); 
+                                        }else if(discountData.percentage && discountData.percentage != '' && discountData.percentage != 0 && !isNaN(discountData.percentage)){
+                                            let per = (bk_total * parseFloat(discountData.percentage)) / 100;
+                                            bk_total = bk_total - parseFloat(per);
+                                        }
+                                        if(parseFloat(total) == parseFloat(bk_total) && parseFloat(notification_amt) == parseFloat(bk_notificationcost) && parseFloat(sms_amt) == parseFloat(bk_smscost) && parseFloat(email_amt) == parseFloat(bk_emailcost)){
+                                            // total match
+                                        }else{
+                                            // total not match
+                                        }
+                                    }else{
+                                        if(parseFloat(total) == parseFloat(bk_total) && parseFloat(notification_amt) == parseFloat(bk_notificationcost) && parseFloat(sms_amt) == parseFloat(bk_smscost) && parseFloat(email_amt) == parseFloat(bk_emailcost)){
+                                            // total match
+                                        }else{
+                                            // total not match
+                                        }
+                                        // dicount not apply
+                                    }
+                                }else{
+                                    if(parseFloat(total) == parseFloat(bk_total) && parseFloat(notification_amt) == parseFloat(bk_notificationcost) && parseFloat(sms_amt) == parseFloat(bk_smscost) && parseFloat(email_amt) == parseFloat(bk_emailcost)){
+                                        // total match
+                                    }else{
+                                        // total not match
+                                    }
+                                    // dicount not apply
+                                }
+                                //return responseManager.onSuccess('Promotion schedule set successfully', {settings : defaultSetting, numberofusers : numberofusers}, res);
+                            } else {
+                                return responseManager.badrequest({ message: 'Something went wrong, please try again' }, res);
+                            }
+                        }else{
+                            // no user selected
                         }
                     } else {
                         return responseManager.badrequest({ message: 'Invalid notification data to set notification user data, please try again' }, res);
